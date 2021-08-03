534d25eb3fd7e0cfc4416d1b1974f261bb166215
Kpazawala <kpazawala@gmail.co>
2021-08-03 16:09:28 -0700

    work

--- public/popup.js
+++ public/popup.js
@@ -1,17 +1,12 @@
-console.log("hello");
+
+
+let bgpage = chrome.extension.getBackgroundPage();
+let image_source = bgpage.image_source;
 
 function setup() {
   console.log("hello");
-
-  let bgpage = chrome.extension.getBackgroundPage();
-  let image_source = bgpage.image_source;
-
+  console.log(image_source);
 }
 
 function draw(){
-  for (var i = 0; i < image_source.length; i++) {
-
-  console.log(image_source);
-  print("hello")
-  }
 }
