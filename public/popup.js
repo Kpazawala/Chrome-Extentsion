b39fd2fb019637769feffb91e1c04a3eb8e0b6a1
Kpazawala <kpazawala@gmail.co>
2021-08-03 16:21:10 -0700

    finally

--- public/popup.js
+++ public/popup.js
@@ -1,12 +1,29 @@
 
-
+let gui;
 let bgpage = chrome.extension.getBackgroundPage();
 let image_source = bgpage.image_source;
 
 function setup() {
   console.log("hello");
   console.log(image_source);
+
+
+  gui = createGui();
+  b = createButton("1", 50, 50)
+  let global galleryArray = [];
 }
 
 function draw(){
+  for (var i = 0; i < image_source.length; i++) {
+
+  console.log(image_source);
+  print("hello")
+  }
+
+  drawGui();
+}
+
+function GalleryDisplay() {
+  for(let image in galleryArray){
+    image.displayImage();
 }
