ae66a1e9d8c5b86c0ace3d848d1eded8fa912dac
Kpazawala <kpazawala@gmail.co>
2021-08-03 17:05:31 -0700

    start

--- public/popup.js
+++ public/popup.js
@@ -1,29 +1,40 @@
 
-let gui;
 let bgpage = chrome.extension.getBackgroundPage();
 let image_source = bgpage.image_source;
+
+let galleryArray;
 
 function setup() {
   console.log("hello");
   console.log(image_source);
 
+  createCanvas(100, 100);
 
-  gui = createGui();
+  galleryArray = [];
+
+  for(var i = 0; i < image_source.length; i++){
+    let img_index = new GalleryImage(image_source[i]);
+    galleryArray.push(img_index);
+    galleryArray[i].displayImage();
+  }
+
   b = createButton("1", 50, 50)
-  let global galleryArray = [];
+
 }
 
 function draw(){
-  for (var i = 0; i < image_source.length; i++) {
-
-  console.log(image_source);
-  print("hello")
-  }
-
-  drawGui();
 }
 
-function GalleryDisplay() {
-  for(let image in galleryArray){
-    image.displayImage();
+class GalleryImage {
+  constructor(image){
+    this.image = image;
+    this.x = 0;
+    this.y = 0;
+    this.imageWidth = 30;
+    this.imageHeight = 35;
+  }
+
+  displayImage() {
+    createImg(this.image, this.x, this.y, this.imageWidth, this.imageHeight);
+  }
 }
